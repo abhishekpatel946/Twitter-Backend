5b420a4badab502dfb3216c4e3b5c26eb5a8a773
Abhishek Patel <abhishekpatel946@gmail.com>
2022-03-22 19:57:00 +0530

    add endpoints for posts controller are done

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  @Get('/')
+  getAllPosts(): string {
+    // TODO: get all posts
+    return 'get all posts';
+  }
+
+  @Get('/:postid')
+  getPostById(@Param() param): string {
+    // TODO: get post by postid
+    return `post info of postid: ${param.postid}`;
+  }
+
+  @Post('/')
+  createNewPost(): string {
+    // TODO: create new post
+    return 'create new post';
+  }
+
+  @Delete('/:postid')
+  deletePostById(@Param() param): string {
+    // TODO: delete post by postid
+    return `delete post of postid: ${param.postid}`;
+  }
+
+  @Put('/:postid/like')
+  likePost(@Param() param): string {
+    // TODO: like post
+    return `like post of postid: ${param.postid}`;
+  }
+
+  @Delete('/:postid/like')
+  unlikePost(@Param() param): string {
+    // TODO: unlike post
+    return `unlike post of postid: ${param.postid}`;
+  }
+}
