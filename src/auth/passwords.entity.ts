f3c006d2a5779b14c746e6a62a377188a9a26d4d
Abhishek Patel <abhishekpatel946@gmail.com>
2022-04-09 11:17:32 +0530

    fix: Password entity issue with last commit

--- src/auth/passwords.entity.ts
+++ src/auth/passwords.entity.ts
@@ -1,7 +1,8 @@
 import { MooBaseEntity } from 'src/commons/base.entity';
 import { UserEntity } from 'src/users/users.entity';
-import { Column, JoinColumn, OneToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
 
+@Entity('passwords')
 export class PasswordEntity extends MooBaseEntity {
   @Column()
   userId: string;
