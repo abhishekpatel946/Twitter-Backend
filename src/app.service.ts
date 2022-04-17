3fa3951265d9539363b703ea96725d91f71a4aa2
Abhishek Patel <abhishekpatel946@gmail.com>
2022-04-17 13:53:17 +0530

    feat: Added the logger for production grade

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  logger: Logger;
+
+  constructor() {
+    this.logger = new Logger(AppService.name);
+  }
+
   getHello(): string {
+    this.logger.debug('Hello world!');
     return 'Hello Nest!';
   }
 }
